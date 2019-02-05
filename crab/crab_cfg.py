4377db623f2e5a0b27bf4b0ffff064ca00eaf412
Elisabetta Manca <elisabetta.manca@cern.ch>
2019-02-05 14:36:05 +0100

    scripts for crab submitting

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -4,30 +4,25 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'NanoPost1'
+config.General.requestName = 'NanoPostWmass'
 config.General.transferLogs=True
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'PSet.py'
 config.JobType.scriptExe = 'crab_script.sh'
-config.JobType.inputFiles = ['crab_script.py','../scripts/haddnano.py'] #hadd nano will not be needed once nano tools are in cmssw
+config.JobType.inputFiles = ['crab_script.py','../scripts/haddnano.py','../python/postprocessing/wmass/keep_and_drop.txt'] #hadd nano will not be needed once nano tools are in cmssw
 config.JobType.sendPythonFolder	 = True
 config.section_("Data")
-config.Data.inputDataset = '/DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17NanoAOD-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/NANOAODSIM'
-#config.Data.inputDBS = 'phys03'
-config.Data.inputDBS = 'global'
+config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/emanca-NanoWMass-b0caab6fd5c76dc4ef8a67bbbb0491fc/USER'
+config.Data.inputDBS = 'phys03'
+#config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 #config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 2
-config.Data.totalUnits = 10
-
-config.Data.outLFNDirBase = '/store/user/%s/NanoPost' % (getUsernameFromSiteDB())
+config.Data.unitsPerJob = 10
+#config.Data.totalUnits = 10
+config.Data.outLFNDirBase = '/store/user/%s/NanoPostWmass' % (getUsernameFromSiteDB())
 config.Data.publication = False
-config.Data.outputDatasetTag = 'NanoTestPost'
+config.Data.outputDatasetTag = 'NanoPostWmass'
 config.section_("Site")
-config.Site.storageSite = "T2_DE_DESY"
-
-#config.Site.storageSite = "T2_CH_CERN"
-#config.section_("User")
-#config.User.voGroup = 'dcms'
+config.Site.storageSite = "T2_IT_Pisa"
 
--- crab/crab_script.py
+++ crab/crab_script.py
@@ -5,8 +5,11 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
 #this takes care of converting the input files from CRAB
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
 
-from  PhysicsTools.NanoAODTools.postprocessing.examples.exampleModule import *
-p=PostProcessor(".",inputFiles(),"Jet_pt>200",modules=[exampleModuleConstr()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis())
+from PhysicsTools.NanoAODTools.postprocessing.wmass.lepSelection import *
+from PhysicsTools.NanoAODTools.postprocessing.wmass.CSVariables import *
+from PhysicsTools.NanoAODTools.postprocessing.wmass.Wproducer import *
+
+p=PostProcessor(".",inputFiles(),"",modules=[leptonSelectModule(),CSAngleModule(), WproducerModule()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="keep_and_drop.txt")
 p.run()
 
 print "DONE"
