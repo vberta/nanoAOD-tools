fd9486e719ee5f5b2f76d6b488d94d17ce60b138
bianchini <lore.bianchini@gmail.com>
2019-02-08 17:18:31 +0100

    CRAB files into crab/ dir

--- crab/crab_script.py
+++ crab/crab_script.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python
-import os
-from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import * 
+import os, sys
+import ROOT
+import argparse
+ROOT.PyConfig.IgnoreCommandLineOptions = True
 
-#this takes care of converting the input files from CRAB
-from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
+
+from PhysicsTools.NanoAODTools.postprocessing.wmass.preselection import *
+from PhysicsTools.NanoAODTools.postprocessing.wmass.additionalVariables import *
 from PhysicsTools.NanoAODTools.postprocessing.wmass.lepSelection import *
 from PhysicsTools.NanoAODTools.postprocessing.wmass.CSVariables import *
 from PhysicsTools.NanoAODTools.postprocessing.wmass.Wproducer import *
 
-p=PostProcessor(".",inputFiles(),"",modules=[leptonSelectModule(),CSAngleModule(), WproducerModule()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="keep_and_drop.txt")
+parser = argparse.ArgumentParser("")
+parser.add_argument('-jobNum', '--jobNum',   type=int, default=1,      help="")
+parser.add_argument('-passall', '--passall', type=int, default=0,      help="")
+parser.add_argument('-isMC', '--isMC',       type=int, default=1,      help="")
+args = parser.parse_args()
+isMC    = args.isMC
+passall = args.passall
+print "isMC =", isMC, ", passall=", passall
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
+
+modules = []
+if isMC:
+    modules = [puAutoWeight(), preselection(isMC=isMC, passall=passall), additionalVariables(), leptonSelectModule(), CSAngleModule(), WproducerModule()]
+else:
+    modules = [preselection(isMC=isMC, passall=passall), additionalVariables()]
+
+p = PostProcessor(outputDir=".",        
+                  inputFiles=inputFiles(),
+                  modules=modules,
+                  provenance=True,
+                  outputbranchsel="keep_and_drop_"+("MC" if isMC else "Data")+".txt",
+                  fwkJobReport=True,
+                  jsonInput=runsAndLumis())
 p.run()
 
 print "DONE"
 os.system("ls -lR")
+
 
