68b3b99146e0def5175048bbb4578bf8a729c0e5
bianchini <lore.bianchini@gmail.com>
2019-02-12 16:08:33 +0100

    bug fixes to Mt and recoils, added JES and JER variations to MET

--- python/postprocessing/wmass/additionalVariables.py
+++ python/postprocessing/wmass/additionalVariables.py
@@ -9,49 +9,29 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
 def evaluateMt(mu_pt, mu_phi, met_pt, met_phi) :
     muvec = TLorentzVector()
-    muvec.SetPtEtaPhiM(mu_pt,0,mu_phi,0)
+    muvec.SetPtEtaPhiM(mu_pt,0.0,mu_phi,0.106)
     metvec = TLorentzVector()
-    metvec.SetPtEtaPhiE(met_pt,0,met_phi,0)
-    deltaphi=metvec.DeltaPhi(muvec)
-    mt = sqrt(2*mu_pt*met_pt*(1-cos(deltaphi)))
-    return mt
-
-def evaluateRecoilPt(mu_pt, mu_phi, met_pt, met_phi) :
-    p_x = mu_pt*sin(mu_phi)
-    p_y = mu_pt*cos(mu_phi)
-    met_x = met_pt*sin(met_phi)
-    met_y = met_pt*cos(met_phi)
-    h_x =  -p_x-met_x
-    h_y =  -p_y-met_y
-    h = sqrt((h_x)**2+(h_y)**2)
-    return h
-
-def evaluateRecoilPhi(mu_phi, met_phi) :
-    hphi = mu_phi+met_phi
-    hphi = hphi % (2*math.pi)
-    return hphi
-
-def evaluateMtRecoBased(mu_pt, mu_phi, met_pt, met_phi) :
-    p_x = mu_pt*sin(mu_phi)
-    p_y = mu_pt*cos(mu_phi)
-    met_x = met_pt*sin(met_phi)
-    met_y = met_pt*cos(met_phi)
-    h_x =  -p_x-met_x
-    h_y =  -p_y-met_y
-    mt_h = sqrt(2*(mu_pt*sqrt((p_x+h_x)**2+(p_y+h_y)**2)+mu_pt**2+p_x*h_x+p_y*h_y))
-    return mt_h
-
+    metvec.SetPtEtaPhiM(met_pt,0.0,met_phi,0.0)
+    hvec = -(muvec + metvec)
+    (mt, h_pt, h_phi, met_par, met_per) = (hvec.M(), hvec.Pt(), hvec.Phi(), 
+                                           metvec.Vect().Dot(muvec.Vect().Unit()), metvec.Vect().Cross(muvec.Vect().Unit()).Pz())
+    return  (mt, h_pt, h_phi, met_par, met_per) 
 
 class additionalVariables(Module):
-    def __init__(self, isMC=True):
+    def __init__(self, isMC=True, doJESVar=False, doJERVar=False, doUnclustVar=False):
         self.metdict = {
-            "pf" : "",
-            #"gen" : "Gen",
-            "tk" : "Tk",
-            "puppi" : "Puppi"
+            "pf"    : { "tag" : "MET",      "var"  : [""] },
+            "tk"    : { "tag" : "TkMET",    "var"  : [""] },
+            "puppi" : { "tag" : "PuppiMET", "var"  : [""] },
             }
         if isMC:
-            self.metdict["gen"] = "Gen"
+            self.metdict["gen"] = {"tag" : "GenMET", "var"  : [""]}
+            if doJERVar:
+                self.metdict["pf"]["var"].extend( ["_nom", "_jerUp", "_jerDown"] )
+            if doJESVar:
+                self.metdict["pf"]["var"].extend( ["_jesTotalUp", "_jesTotalDown"] )
+            if doUnclustVar:
+                self.metdict["pf"]["var"].extend( ["_unclustEnUp", "_unclustEnDown"] )
         pass
 
     def beginJob(self):
@@ -60,10 +40,13 @@ class additionalVariables(Module):
         pass
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.out = wrappedOutputTree
-        for met in self.metdict :
-            self.out.branch("Muon_mt_%s" % self.metdict[met], "F", lenVar="nMuons")
-            self.out.branch("Muon_recoil_pt_%s" % self.metdict[met], "F", lenVar="nMuons")
-            self.out.branch("Muon_recoil_phi_%s" % self.metdict[met], "F", lenVar="nMuons")
+        for key,met in self.metdict.items() :
+            for ivar,var in enumerate(met["var"]):
+                self.out.branch("Muon_%s%s_mt" % (met["tag"], var), "F", lenVar="nMuons", title="Transverse mass using "+met["tag"]+" syst "+var+" MET")
+                self.out.branch("Muon_%s%s_recoil_pt"  % (met["tag"], var), "F", lenVar="nMuons", title="Recoil Pt using "+met["tag"]+" syst "+var+" MET")
+                self.out.branch("Muon_%s%s_recoil_phi" % (met["tag"], var), "F", lenVar="nMuons", title="Recoil Phi using "+met["tag"]+" syst "+var+" MET")
+                self.out.branch("Muon_%s%s_met_par" % (met["tag"], var), "F", lenVar="nMuons", title="MET projected along mu using "+met["tag"]+" syst "+var+" MET")
+                self.out.branch("Muon_%s%s_met_per" % (met["tag"], var), "F", lenVar="nMuons", title="MET perpendicular to mu using "+met["tag"]+" syst "+var+" MET")
         pass
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
@@ -74,33 +57,29 @@ class additionalVariables(Module):
         #collections
         muons = Collection(event, "Muon")
 
-        for met in self.metdict :
+        #final vectors
+        Mt_vec         = [0.0]*len(muons)
+        Recoil_pt_vec  = [0.0]*len(muons)
+        Recoil_phi_vec = [0.0]*len(muons)
+        met_par_vec    = [0.0]*len(muons)
+        met_per_vec    = [0.0]*len(muons)
 
-            #collections
-            met_pt = getattr(event,"%sMET_pt" % self.metdict[met])
-            met_phi = getattr(event,"%sMET_phi" % self.metdict[met])
-
-            #final vectors
-            Mt_vec = []
-            Recoil_pt_vec = []
-            Recoil_phi_vec = []
-
-            for mu in muons :
-
-                #transverse mass
-                mt = evaluateMt(mu.pt, mu.phi,met_pt,met_phi)
-
-                #recoil
-                h = evaluateRecoilPt(mu.pt, mu.phi,met_pt,met_phi)
-                hphi = evaluateRecoilPhi( mu.phi,met_phi)
-
-                Mt_vec.append(mt)
-                Recoil_pt_vec.append(h)
-                Recoil_phi_vec.append(hphi)
-
-            self.out.fillBranch("Muon_mt_%s" % self.metdict[met], Mt_vec)
-            self.out.fillBranch("Muon_recoil_pt_%s" % self.metdict[met], Recoil_pt_vec)
-            self.out.fillBranch("Muon_recoil_phi_%s" % self.metdict[met], Recoil_phi_vec)
+        for key,met in self.metdict.items() :
+            for ivar,var in enumerate(met["var"]):
+                met_pt  = getattr(event,"%s_pt%s"  % (met["tag"], var))
+                met_phi = getattr(event,"%s_phi%s" % (met["tag"], var))
+                for imu,mu in enumerate(muons) :
+                    (mt,hpt,hphi,met_par,met_per) = evaluateMt(mu.pt, mu.phi, met_pt, met_phi)
+                    Mt_vec[imu] = mt
+                    Recoil_pt_vec[imu] = hpt
+                    Recoil_phi_vec[imu] = hphi
+                    met_par_vec[imu] = met_par
+                    met_per_vec[imu] = met_per
+                self.out.fillBranch("Muon_%s%s_mt" % (met["tag"], var), Mt_vec)
+                self.out.fillBranch("Muon_%s%s_recoil_pt"  % (met["tag"], var), Recoil_pt_vec)
+                self.out.fillBranch("Muon_%s%s_recoil_phi" % (met["tag"], var), Recoil_phi_vec)
+                self.out.fillBranch("Muon_%s%s_met_par" % (met["tag"], var), met_par_vec)
+                self.out.fillBranch("Muon_%s%s_met_per" % (met["tag"], var), met_per_vec)
 
         return True
 
