e550b5c11e561cec9a4f509a5bf9e291b78ffac8
bianchini <lore.bianchini@gmail.com>
2019-02-14 15:07:29 +0100

    moved  LeptonEff to data/, extra features in crab_cfg, added all mcsamples for 2016

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -3,16 +3,31 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 import argparse
 import sys
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 parser = argparse.ArgumentParser("")
-parser.add_argument('-tag', '--tag',          type=str, default="TEST",      help="")
-parser.add_argument('-isMC', '--isMC',        type=int, default=1,      help="")
+parser.add_argument('-tag', '--tag', type=str, default="TEST",      help="")
+parser.add_argument('-isMC', '--isMC', type=int, default=1,      help="")
 parser.add_argument('-dataYear', '--dataYear',type=int, default=2016, help="")
+parser.add_argument('-run', '--run', type=str, default="submit", help="")
 args = parser.parse_args()
 tag = args.tag
 isMC = args.isMC
 dataYear = args.dataYear
+run = args.run
 samples = ('mc' if isMC else 'data')+'samples_'+str(dataYear)+'.txt'
-print "tag =", tag, ", isMC =", isMC, ", samples =", samples, ", dataYear =", dataYear
+print "tag =", bcolors.OKGREEN, tag, bcolors.ENDC, \
+    ", isMC =", bcolors.OKGREEN, str(isMC), bcolors.ENDC, \
+    ", dataYear =", bcolors.OKGREEN, str(dataYear), bcolors.ENDC, \
+    " => running on sample file:", bcolors.OKGREEN, samples, bcolors.ENDC
 
 config = Configuration()
 
@@ -36,8 +51,7 @@ if not isMC:
     if dataYear==2016:
         config.Data.lumiMask = 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
     else:
-        # FIXME
-        config.Data.lumiMask = 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+        config.Data.lumiMask = 'Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'
     print "Using lumiMask", config.Data.lumiMask
 config.Data.outLFNDirBase = '/store/user/%s/NanoAOD-%s' % (getUsernameFromSiteDB(), tag)
 config.Data.publication = False
@@ -62,16 +76,21 @@ if __name__ == '__main__':
         if dataset[0]=='#':
             continue
         dataset_inputDataset = dataset.split(',')[0]
-        dataset_unitsPerJob = dataset.split(',')[1]
+        dataset_unitsPerJob = int(dataset.split(',')[1])
         print 'inputDataset: '+dataset_inputDataset
-        print 'unitsPerJob:  '+dataset_unitsPerJob
+        print 'unitsPerJob:  '+str(dataset_unitsPerJob)
         config.Data.inputDataset = dataset_inputDataset
         config.Data.unitsPerJob = dataset_unitsPerJob
-        config.General.requestName = dataset.split('/')[1]+'_sub'+str(n)
+        config.General.requestName = dataset.split('/')[1]+'_task'+str(n)
         config.Data.outputDatasetTag = dataset.split('/')[2]
         print 'requestName:  '+config.General.requestName
-        print 'output  ===>  '+config.Data.outLFNDirBase+'/'+dataset.split('/')[1]+'/'+config.Data.outputDatasetTag
-        #crabCommand('submit', '--dryrun', config=config)
+        print 'output  ===> ', bcolors.OKBLUE , config.Data.outLFNDirBase+'/'+dataset.split('/')[1]+'/'+config.Data.outputDatasetTag+'/' , bcolors.ENDC
+        if run=='submit':
+            crabCommand('submit', config=config)
+        elif run=='dryrun':
+            crabCommand('submit', '--dryrun', config=config)
+        elif run=='debug':            
+            print "Doing nothing with CRAB. Create postcrab file"
         crablog = open('crab_'+config.General.requestName+'/crab.log', 'r').readlines()
         crabloglines = [x.strip() for x in crablog]
         username = getUsernameFromSiteDB()
