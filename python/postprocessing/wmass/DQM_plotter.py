c36e509538221e31d3e92fccd6d177c05e9b8262
Valerio Bertacchi <valerio.bertacchi@gmail.com>
2019-02-27 12:42:36 +0100

    version1 of DQM and xsec mc2016

--- python/postprocessing/wmass/DQM_plotter.py
+++ python/postprocessing/wmass/DQM_plotter.py
@@ -1,19 +1,24 @@
 from ROOT import *
+from ROOT import gStyle
 import argparse
 gROOT.Reset()
 gROOT.SetBatch(True)
+gStyle.SetOptStat("eMRuoi")  #(012211100)
 from definitions import variables
 
 
 parser = argparse.ArgumentParser("Fast Plotter for postprocessing")
 parser.add_argument('-Nevents',     '--Nevents',        type=int,  default=100,               action='store', help='number of events to plot')
 parser.add_argument('-inputFile',   '--inputFile',      type=str,                             action='store', help='input file. No default provided')
-parser.add_argument('-outputFile',   '--outputFile',    type=str, default='DQM_output.root',  action='store', help='output file name')
+parser.add_argument('-outputFile',  '--outputFile',     type=str, default='DQM_output.root',  action='store', help='output file name')
+parser.add_argument('-dataset',     '--dataset',        type=str, default='dataset',          action='store', help='dataset name')
 
 args = parser.parse_args()
 Nevents     = args.Nevents
 inputFile   = args.inputFile
 outputFile  = args.outputFile
+dataset     = args.dataset
+
 
 myfile = TFile(inputFile)
 mytree = myfile.Events
@@ -23,7 +28,7 @@ print("tree entries",mytree.GetEntries())
 variables
 canvaslist = {}
 histolist = {}
-outputFile = TFile("outputFile","recreate")
+output = TFile(outputFile,"recreate")
 
 #dimuon????? check!
 for key_part, part in variables.items() :
@@ -37,15 +42,24 @@ for key_part, part in variables.items() :
 
         hName = key_part+'_'+var["name"]
         histolist[hName] = TH1F(hName,hName,var["Nbin"],var["min"],var["max"])
-        print(hName, var["name"])
+        # print(hName, var["name"])
         mytree.Project(hName,var["name"],"Vtype=="+goodV,"",Nevents)
-        print(histolist[hName].GetEntries())
+        # print(histolist[hName].GetEntries())
         histolist[hName].GetXaxis().SetTitle(var["title"])
 
         canvaslist['c_'+key_part+'_'+key_var] = TCanvas('c_'+key_part+'_'+key_var,'c_'+key_part+'_'+key_var, 800,600)
         canvaslist['c_'+key_part+'_'+key_var].cd()
         histolist[hName].Draw()
-        canvaslist['c_'+key_part+'_'+key_var].SaveAs(key_part+'_'+key_var+'.png')
+        canvaslist['c_'+key_part+'_'+key_var].SetGrid()
 
-        outputFile.cd()
+        legSample = TLegend(0.1,0.95,0.7,0.9);
+        legSample.SetHeader(dataset)
+        legSample.SetBorderSize(0)
+        legSample.Draw("SAME")
+        legSample.SetFillStyle(0);
+
+        canvaslist['c_'+key_part+'_'+key_var].SaveAs(dataset+'_'+key_part+'_'+key_var+'.png')
+
+        output.cd()
+        canvaslist['c_'+key_part+'_'+key_var].Write()
         histolist[hName].Write()
--- python/postprocessing/wmass/definitions.py
+++ python/postprocessing/wmass/definitions.py
@@ -114,48 +114,48 @@ variables =  {
     #             'max' : 100
     #             }
     'W' : {
-        'Muon_pt' : HistoDict(('Muon%s_pt[Idx_mu1]' % muon),"$P_T$ [GeV]",100,-1000,1000), #TEMPORARY
-        'Muon_eta' : HistoDict(('Muon%s_eta[Idx_mu1]' % muon),"$\eta$",100,-1000,1000), #TEMPORARY
+        'Muon_pt' : HistoDict(('Muon%s_pt[Idx_mu1]' % muon),"$P_{T}$ [GeV]",100,0,200), #TEMPORARY
+        'Muon_eta' : HistoDict(('Muon%s_eta[Idx_mu1]' % muon),"$\eta$",100,-4,4), #TEMPORARY
         'Muon_phi' : HistoDict(('Muon%s_phi[Idx_mu1]' % muon),"$\phi$",100,-math.pi,math.pi),
-        'Muon_pfRelIso04' : HistoDict(('Muon%s_pfRelIso04_all[Idx_mu1]' % muon),"relIso ",100,-1000,1000), #TEMPORARY
-        'Muon_dxy' : HistoDict(('Muon%s_dxy[Idx_mu1]' % muon),"$d_{xy}$ [cm]",100,-1000,1000),#TEMPORARY
-        'dz' : HistoDict(('Muon%s_dz[Idx_mu1]' % muon),"$d_z$ [cm]",100,-1000,1000),#TEMPORARY
-        'Muon_MET_mt' : HistoDict(('Muon%s_MET%s_mt[Idx_mu1]' % (muon, met)),"$M_T$ [GeV]",100,-1000,1000), #TEMPORARY
-        'Muon_MET_h_pt' : HistoDict(('Muon%s_MET%s_h_pt[Idx_mu1]' % (muon, met)),"$P_T$ [GeV]",100,-1000,1000), #TEMPORARY
-        'GenMuon_bare_GenMET_mt' : HistoDict(('GenMuon_bare_GenMET_mt[Idx_mu1]'),"P_T [GeV]",100,-1000,1000), #TEMPORARY
-        'GenMuon_bare_GenMET_h_pt' : HistoDict(('GenMuon_bare_GenMET_h_pt[Idx_mu1]'),"P_T [GeV]",100,-1000,1000), #TEMPORARY
-        'MET_pt' : HistoDict(('MET%s_pt' % met),"P_T [GeV]",100,-1000,1000), #TEMPORARY
-        'Muon_MET_uPar' : HistoDict(('Muon_MET%s_uPar' % met),"P_T [GeV]",100,-1000,1000), #TEMPORARY
-        'Muon_MET_uPer' : HistoDict(('Muon_MET%s_uPer' % met),"P_T [GeV]",100,-1000,1000), #TEMPORARY
-        'PV_npvsGood' : HistoDict('PV_npvsGood',"N",100,-1000,1000), #TEMPORARY
+        'Muon_pfRelIso04' : HistoDict(('Muon%s_pfRelIso04_all[Idx_mu1]' % muon),"relIso ",100,0,0.4), #TEMPORARY
+        'Muon_dxy' : HistoDict(('Muon%s_dxy[Idx_mu1]' % muon),"d_{xy} [cm]",100,-0.01,0.01),#TEMPORARY
+        'dz' : HistoDict(('Muon%s_dz[Idx_mu1]' % muon),"$d_z$ [cm]",100,-0.1,0.1),#TEMPORARY
+        'Muon_MET_mt' : HistoDict(('Muon%s_MET%s_mt[Idx_mu1]' % (muon, met)),"$M_{T}$ [GeV]",100,0,200), #TEMPORARY
+        'Muon_MET_h_pt' : HistoDict(('Muon%s_MET%s_h_pt[Idx_mu1]' % (muon, met)),"$P_{T}$ [GeV]",100,0,200), #TEMPORARY
+        'GenMuon_bare_GenMET_mt' : HistoDict(('GenMuon_bare_GenMET_mt[Idx_mu1]'),"P_{T} [GeV]",100,0,200), #TEMPORARY
+        'GenMuon_bare_GenMET_h_pt' : HistoDict(('GenMuon_bare_GenMET_h_pt[Idx_mu1]'),"P_{T} [GeV]",100,0,200), #TEMPORARY
+        'MET_pt' : HistoDict(('MET%s_pt' % met),"P_{T} [GeV]",100,0,200), #TEMPORARY
+        'Muon_MET_uPar' : HistoDict(('Muon_MET%s_uPar' % met),"P_{T} [GeV]",100,-200,200), #TEMPORARY
+        'Muon_MET_uPer' : HistoDict(('Muon_MET%s_uPer' % met),"P_{T} [GeV]",100,-200,200), #TEMPORARY
+        'PV_npvsGood' : HistoDict('PV_npvsGood',"N",100,0,100), #TEMPORARY
     },
 
     'Z' : {
-        'Muon_pt_1' : HistoDict(('Muon%s_pt[Idx_mu1]' % muon),"$P_T$ [GeV]",100,-1000,1000), #TEMPORARY
-        'Muon_eta_1' : HistoDict(('Muon%s_eta[Idx_mu1]' % muon),"$\eta$",100,-1000,1000), #TEMPORARY
+        'Muon_pt_1' : HistoDict(('Muon%s_pt[Idx_mu1]' % muon),"$P_{T}$ [GeV]",100,0,200), #TEMPORARY
+        'Muon_eta_1' : HistoDict(('Muon%s_eta[Idx_mu1]' % muon),"$\eta$",100,-4,4), #TEMPORARY
         'Muon_phi_1' : HistoDict(('Muon%s_phi[Idx_mu1]' % muon),"$\phi$",100,-math.pi,math.pi),
-        'Muon_pfRelIso04_1' : HistoDict(('Muon%s_pfRelIso04_all[Idx_mu1]' % muon),"relIso ",100,-1000,1000), #TEMPORARY
-        'Muon_dxy_1' : HistoDict(('Muon%s_dxy[Idx_mu1]' % muon),"$d_{xy}$ [cm]",100,-1000,1000),#TEMPORARY
-        'dz_1' : HistoDict(('Muon%s_dz[Idx_mu1]' % muon),"$d_z$ [cm]",100,-1000,1000),#TEMPORARY
-        'Muon_pt_2' : HistoDict(('Muon%s_pt[Idx_mu2]' % muon),"$P_T$ [GeV]",100,-1000,1000), #TEMPORARY
-        'Muon_eta_2' : HistoDict(('Muon%s_eta[Idx_mu2]' % muon),"$\eta$",100,-1000,1000), #TEMPORARY
+        'Muon_pfRelIso04_1' : HistoDict(('Muon%s_pfRelIso04_all[Idx_mu1]' % muon),"relIso ",100,0,0.4), #TEMPORARY
+        'Muon_dxy_1' : HistoDict(('Muon%s_dxy[Idx_mu1]' % muon),"d_{xy} [cm]",100,-0.01,0.01),#TEMPORARY
+        'dz_1' : HistoDict(('Muon%s_dz[Idx_mu1]' % muon),"$d_z$ [cm]",100,-0.1,0.1),#TEMPORARY
+        'Muon_pt_2' : HistoDict(('Muon%s_pt[Idx_mu2]' % muon),"$P_{T}$ [GeV]",100,0,200), #TEMPORARY
+        'Muon_eta_2' : HistoDict(('Muon%s_eta[Idx_mu2]' % muon),"$\eta$",100,-4,4), #TEMPORARY
         'Muon_phi_2' : HistoDict(('Muon%s_phi[Idx_mu2]' % muon),"$\phi$",100,-math.pi,math.pi),
-        'Muon_pfRelIso04_2' : HistoDict(('Muon%s_pfRelIso04_all[Idx_mu2]' % muon),"relIso ",100,-1000,1000), #TEMPORARY
-        'Muon_dxy_2' : HistoDict(('Muon%s_dxy[Idx_mu2]' % muon),"$d_{xy}$ [cm]",100,-1000,1000),#TEMPORARY
-        'dz_2' : HistoDict(('Muon%s_dz[Idx_mu2]' % muon),"$d_z$ [cm]",100,-1000,1000),#TEMPORARY
-        'RecoZ_mass': HistoDict(('RecoZ%s_mass' % muon),"M [GeV]",100,-1000,1000), #TEMPORARY
-        'RecoZ_y': HistoDict(('RecoZ%s_y' % muon),"y",100,-1000,1000), #TEMPORARY
-        'RecoZ_pt': HistoDict(('RecoZ%s_pt' % muon),"P_T [GeV]",100,-1000,1000), #TEMPORARY
-        'RecoZ_CStheta': HistoDict(('RecoZ%s_CStheta' % muon),"$\theta$",100,-math.pi,math.pi),
-        'RecoZ_CSphi': HistoDict(('RecoZ%s_CSphi' % muon),"$\phi$",100,-math.pi,math.pi),
-        'GenZ_mass' : HistoDict(('GenZ%s_mass' % muon),"M [GeV]",100,-1000,1000), #TEMPORARY
-        'GenZ_y': HistoDict(('GenZ%s_y' % muon),"y",100,-1000,1000), #TEMPORARY
-        'GenZ_pt' : HistoDict(('GenZ%s_pt' % muon),"P_T [GeV]",100,-1000,1000), #TEMPORARY
-        'GenZ_CStheta' : HistoDict(('GenZ%s_CStheta' % muon),"$\theta$",100,-math.pi,math.pi), #TEMPORARY
-        'GenZ_CSphi'  : HistoDict(('GenZ%s_CSphi' % muon),"$\phi$",100,-math.pi,math.pi), #TEMPORARY
-        'RecoZ_MET_uPar' : HistoDict(('RecoZ_MET%s_uPar' % met),"P_T [GeV]",100,-1000,1000), #TEMPORARY
-        'RecoZ_MET_uPer' : HistoDict(('RecoZ_MET%s_uPer' % met),"P_T [GeV]",100,-1000,1000), #TEMPORARY
-        'PV_npvsGood' : HistoDict('PV_npvsGood',"N",100,-1000,1000), #TEMPORARY
+        'Muon_pfRelIso04_2' : HistoDict(('Muon%s_pfRelIso04_all[Idx_mu2]' % muon),"relIso ",100,0,0.4), #TEMPORARY
+        'Muon_dxy_2' : HistoDict(('Muon%s_dxy[Idx_mu2]' % muon),"d_{xy} [cm]",100,-0.01,0.01),#TEMPORARY
+        'dz_2' : HistoDict(('Muon%s_dz[Idx_mu2]' % muon),"$d_z$ [cm]",100,-0.1,0.1),#TEMPORARY
+        'RecoZ_mass': HistoDict(('RecoZ%s_mass' % muon),"M [GeV]",100,0,200), #TEMPORARY
+        'RecoZ_y': HistoDict(('RecoZ%s_y' % muon),"y",100,-10,10), #TEMPORARY
+        'RecoZ_pt': HistoDict(('RecoZ%s_pt' % muon),"P_{T} [GeV]",100,0,200), #TEMPORARY
+        'RecoZ_CStheta': HistoDict(('RecoZ%s_CStheta' % muon),"$\Theta$",100,-math.pi,math.pi),
+        'RecoZ_CSphi': HistoDict(('RecoZ%s_CSphi' % muon),"$\phi$",100,0,2*math.pi),
+        'GenZ_mass' : HistoDict(('GenZ%s_mass' % muon),"M [GeV]",100,0,200), #TEMPORARY
+        'GenZ_y': HistoDict(('GenZ%s_y' % muon),"y",100,-200,200), #TEMPORARY
+        'GenZ_pt' : HistoDict(('GenZ%s_pt' % muon),"P_{T} [GeV]",100,-200,200), #TEMPORARY
+        'GenZ_CStheta' : HistoDict(('GenZ%s_CStheta' % muon),"$\Theta$",100,-math.pi,math.pi), #TEMPORARY
+        'GenZ_CSphi'  : HistoDict(('GenZ%s_CSphi' % muon),"$\phi$",100,0,2*math.pi), #TEMPORARY
+        'RecoZ_MET_uPar' : HistoDict(('RecoZ_MET%s_uPar' % met),"P_{T} [GeV]",100,-200,200), #TEMPORARY
+        'RecoZ_MET_uPer' : HistoDict(('RecoZ_MET%s_uPer' % met),"P_{T} [GeV]",100,-200,200), #TEMPORARY
+        'PV_npvsGood' : HistoDict('PV_npvsGood',"N",100,0,100), #TEMPORARY
 
     },
 }
