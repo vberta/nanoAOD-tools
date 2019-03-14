36b3cd7225085f30a764479ffb2b12fc6cd8fd22
bianchini <lore.bianchini@gmail.com>
2019-03-14 10:02:12 +0100

    added more tests

--- python/postprocessing/wmass/run_test.sh
+++ python/postprocessing/wmass/run_test.sh
@@ -5,6 +5,7 @@ python postproc.py --isMC 1 --passall 0 --dataYear 2016 --maxEvents 1000 -redoje
 python postproc.py --isMC 0 --passall 0 --dataYear 2016 --maxEvents 10000 -redojec 0
 python postproc.py --isMC 0 --passall 0 --dataYear 2016 --maxEvents 10000 -redojec 1
 python postproc.py --isMC 1 --passall 0 --dataYear 2016 --maxEvents 1000 -redojec 0 --genOnly 1
+python postproc.py --isMC 1 --passall 0 --dataYear 2016 --maxEvents 1000 -redojec 0 --trigOnly 1
 python postproc.py --isMC 1 --passall 0 --dataYear 2017 --maxEvents 1000 -redojec 0
 python postproc.py --isMC 1 --passall 0 --dataYear 2017 --maxEvents 1000 -redojec 1
 python postproc.py --isMC 0 --passall 0 --dataYear 2017 --maxEvents 10000 -redojec 0
@@ -12,3 +13,4 @@ python postproc.py --isMC 0 --passall 0 --dataYear 2017 --maxEvents 10000 -redoj
 python postproc.py --isMC 1 --passall 0 --dataYear 2018 --maxEvents 1000 -redojec 0
 python postproc.py --isMC 1 --passall 0 --dataYear 2018 --maxEvents 1000 -redojec 1
 python postproc.py --isMC 0 --passall 0 --dataYear 2018 --maxEvents 10000 -redojec 0
+python postproc.py --isMC 0 --passall 0 --dataYear 2018 --maxEvents 10000 -redojec 1
