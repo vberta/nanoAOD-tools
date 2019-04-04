dbb6eb1a800d50598dd0c38972f9d46c308bec34
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2019-04-04 16:50:59 +0200

    updates for new jer files available

--- python/postprocessing/wmass/postproc.py
+++ python/postprocessing/wmass/postproc.py
@@ -76,58 +76,9 @@ if crab:
     from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
 
 ################################################ JEC
-"""
-# JEC for MET
-jecTagsMC = {'2016' : 'Summer16_07Aug2017_V11_MC', 
-             '2017' : 'Fall17_17Nov2017_V32_MC', 
-             '2018' : 'Autumn18_V8_MC'}
-
-jecTagsDATA = { '2016B' : 'Summer16_07Aug2017BCD_V11_DATA', 
-                '2016C' : 'Summer16_07Aug2017BCD_V11_DATA', 
-                '2016D' : 'Summer16_07Aug2017BCD_V11_DATA', 
-                '2016E' : 'Summer16_07Aug2017EF_V11_DATA', 
-                '2016F' : 'Summer16_07Aug2017EF_V11_DATA', 
-                '2016G' : 'Summer16_07Aug2017GH_V11_DATA', 
-                '2016H' : 'Summer16_07Aug2017GH_V11_DATA', 
-                '2016H' : 'Summer16_07Aug2017GH_V11_DATA', 
-                '2017B' : 'Fall17_17Nov2017B_V32_DATA', 
-                '2017C' : 'Fall17_17Nov2017C_V32_DATA', 
-                '2017D' : 'Fall17_17Nov2017DE_V32_DATA', 
-                '2017E' : 'Fall17_17Nov2017DE_V32_DATA', 
-                '2017F' : 'Fall17_17Nov2017F_V32_DATA', 
-                '2018A' : 'Autumn18_RunA_V8_DATA',
-                '2018B' : 'Autumn18_RunB_V8_DATA',
-                '2018C' : 'Autumn18_RunC_V8_DATA',
-                '2018D' : 'Autumn18_RunD_V8_DATA',
-                }   
-
-jecTag = jecTagsMC[str(dataYear)] if isMC else jecTagsDATA[str(dataYear) + runPeriod]
-
-jmeUncert = [x for x in jesUncert.split(",")]
-
-print "JECTag =", bcolors.OKGREEN, jecTag,  bcolors.ENDC, \
+"""print "JECTag =", bcolors.OKGREEN, jecTag,  bcolors.ENDC, \
     "jesUncertainties =", bcolors.OKGREEN, jmeUncert,  bcolors.ENDC, \
     "redoJec =", bcolors.OKGREEN, redojec,  bcolors.ENDC
-
-#untar the zipped jec files when submitting crab jobs
-if crab :
-    jesDatadir = os.environ['CMSSW_BASE'] + "/src/PhysicsTools/NanoAODTools/data/jme/"
-    jesInputFile = jesDatadir + jecTag + ".tar.gz"
-    if os.path.isfile(jesInputFile):
-        print "Using JEC files from: %s" % jesInputFile
-        subprocess.call(['tar', "-xzvf", jesInputFile, "-C", jesDatadir])
-    else:
-        print "JEC file %s does not exist" % jesInputFile
-
-#jme corrections
-if isMC:
-    jmeCorrections = lambda : jetmetUncertaintiesProducer(era=str(dataYear), globalTag=jecTag, jesUncertainties=jmeUncert, redoJEC=redojec, saveJets=False)
-else:
-    if redojec:
-        jmeCorrections = lambda : jetRecalib(globalTag=jecTag, saveJets=False)
-    else:
-        jmeCorrections = None
-        print bcolors.OKBLUE, "No module %s will be run" % "jetRecalib", bcolors.ENDC
 """
 #Function definition
 #createJMECorrector(isMC=True, dataYear=2016, runPeriod="B", jesUncert="Total", redoJec=True, saveJets=False, crab=False)
@@ -217,8 +168,6 @@ elif dataYear == 2018:
     lepSFTrig = lambda : lepSFProducerV2(lepFlavour="Muon", cut = "Trigger", histos=triggerHisto[dataYear], dataYear=str(dataYear), runPeriod="ABCD")
     lepSFID   = lambda : lepSFProducerV2(lepFlavour="Muon", cut = "ID", histos=idHisto[dataYear], dataYear=str(dataYear), runPeriod="ABCD", useAbseta=useAbsEta[dataYear], ptEtaAxis=ptEtaAxis[dataYear])
     lepSFISO  = lambda : lepSFProducerV2(lepFlavour="Muon", cut = "ISO", histos=isoHisto[dataYear], dataYear=str(dataYear), runPeriod="ABCD", useAbseta=useAbsEta[dataYear], ptEtaAxis=ptEtaAxis[dataYear])
-
-muModules=[lepSFISO(), lepSFID(), lepSFTrig()]
 ################################################ GEN
 
 Wtypes = ['bare', 'preFSR', 'dress']
